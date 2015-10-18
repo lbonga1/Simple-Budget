23b8f367de2550191a5a74655ca0e3a144bf236d
Lauren Bongartz <lbongartz@me.com>
2015-10-18 16:25:54 -0700

    Budget Cells
    Layout for budget prototype cells

--- Simple Budget/BudgetViewController.swift
+++ Simple Budget/BudgetViewController.swift
@@ -29,62 +29,83 @@ class BudgetViewController: UIViewController {
         self.presentViewController(controller, animated: true, completion: nil)
     }
     
-    func defaultCategories() -> [String: AnyObject] {
-        return  [
-            [
-                "title" : "Savings",
-                "dollarMmount" : 0.00,
-                "percentage" : 0,
-            ], [
-                "title" : "Housing",
-                "dollarAmount" : 0.00,
-                "percentage" : 0,
-            ], [
-                "title" : "Transportation",
-                "dollarAmount" : 0.00,
-                "percentage" : 0,
-            ], [
-                "title" : "Food",
-                "dollarAmount" : 0.00,
-                "percentage" : 0,
-            ], [
-                "title" : "Lifestyle",
-                "dollarAmount" : 0.00,
-                "percentage" : 0,
-            ], [
-                "title" : "Insurance & Tax",
-                "dollarAmount" : 0.00,
-                "percentage" : 0,
-            ], [
-                "title" : "Debt",
-                "dollarAmount" : 0.00,
-                "percentage" : 0,
-            ]
-        ]
+//    func defaultCategories() -> [String: AnyObject] {
+//        return  [
+//            [
+//                "title" : "Savings",
+//                "dollarMmount" : 0.00,
+//                "percentage" : 0,
+//            ], [
+//                "title" : "Housing",
+//                "dollarAmount" : 0.00,
+//                "percentage" : 0,
+//            ], [
+//                "title" : "Transportation",
+//                "dollarAmount" : 0.00,
+//                "percentage" : 0,
+//            ], [
+//                "title" : "Food",
+//                "dollarAmount" : 0.00,
+//                "percentage" : 0,
+//            ], [
+//                "title" : "Lifestyle",
+//                "dollarAmount" : 0.00,
+//                "percentage" : 0,
+//            ], [
+//                "title" : "Insurance & Tax",
+//                "dollarAmount" : 0.00,
+//                "percentage" : 0,
+//            ], [
+//                "title" : "Debt",
+//                "dollarAmount" : 0.00,
+//                "percentage" : 0,
+//            ]
+//        ]
+//    }
+    
+}
+
+// MARK: - Table view data source
+    
+    extension BudgetViewController: UITableViewDataSource {
+
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
     }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return 1
+    }
 
-    // MARK: - Table view data source
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("BudgetCell", forIndexPath: indexPath) as! UITableViewCell
 
-//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        // Return the number of sections.
-//        return 0
-//    }
-
-//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // Return the number of rows in the section.
-//        return 0
-//    }
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
-
-        // Configure the cell...
+        cell.textLabel?.text = "Test"
 
         return cell
+        }
+        
+    func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerCell = tableView.dequeueReusableCellWithIdentifier("HeaderCell") as! CustomHeaderCell
+        
+        headerCell.titleLabel.text = "Test title"
+        
+        return headerCell
+        
+        }
+        
+    func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let footerView = UIView(frame: CGRectMake(0, 0, tableView.frame.size.width, 20))
+        footerView.backgroundColor = UIColor.lightGrayColor()
+            
+        return footerView
     }
-    */
+        
+    func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 20.0
+    }
 
     /*
     // Override to support conditional editing of the table view.
@@ -106,29 +127,10 @@ class BudgetViewController: UIViewController {
     }
     */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
 
-    }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
-    }
-    */
+
 
 }
